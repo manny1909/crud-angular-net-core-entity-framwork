5c2a63c1f4c5f4af89f2148cf13017fe7ce625e4
jhonny <jhonnyherrera1998@gmail.com>
2021-10-07 17:32:04 -0500

    Backend: primer commit
    se creó el proyecto, se instalaron dependencias y se creo el modelo Tarjeta_Credito.

---
+++ Backend/Backend/Models/Tarjeta_Credito.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class Tarjeta_Credito
+    {
+        public int Id { get; set; }
+        [Required] //con dataAnnotations por medio de [Required,Key, etc.] se puede configurar el campo para que no sea nulo, para que sea primary key o foraign 
+        public string Titular { get; set; }
+        [Required]
+        public string Numero_Tarjeta { get; set; }
+        [Required]
+        public string Fecha_Expiracion { get; set; }
+        [Required]
+        public string CVV { get; set; }
+    }
+}
